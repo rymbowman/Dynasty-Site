c7c8ad1927906e81e58965070c5fae7421c1b0f9
Ryan Bowman <rymbowman@gmail.com>
2025-04-02 13:23:56 -0400

    Import AppBar component from MUI into Navbar.jsx and insert imported personal image for league logo

--- flames-fantasy/src/components/navbar/Navbar.jsx
+++ flames-fantasy/src/components/navbar/Navbar.jsx
@@ -1,5 +1,21 @@
+import { AppBar, Box, Container, Toolbar } from "@mui/material";
+import AdbIcon from "@mui/icons-material/Adb";
+import MenuIcon from "@mui/icons-material/Menu";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Box
+            component={"img"}
+            src="/src/assets/images/lu-logo.png"
+            sx={{ width: "3rem", height: "3rem" }}
+          ></Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 };
 
 export default Navbar;
