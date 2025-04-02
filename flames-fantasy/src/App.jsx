0e046d2c193c9a1937464a9a2e73dffd74d23726
Ryan Bowman <rymbowman@gmail.com>
2025-04-02 10:37:17 -0400

    Set up React Router with basic routes and suspense fallback

--- flames-fantasy/src/App.jsx
+++ flames-fantasy/src/App.jsx
@@ -1,7 +1,27 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 import "./App.css";
+import { Suspense } from "react";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+    </Route>
+  )
+);
 function App() {
-  return <div></div>;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <RouterProvider router={router} />
+    </Suspense>
+  );
 }
 
 export default App;
