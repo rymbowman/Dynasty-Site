78c38499b1cb0027f43e7e07648e11f5174acc9e
Ryan Bowman <rymbowman@gmail.com>
2025-04-07 13:47:08 -0400

    Create Navbar component and import the Logo and MenuOptions components into it

--- dynasty-site/src/components/navbar/Navbar.jsx
+++ dynasty-site/src/components/navbar/Navbar.jsx
@@ -1,5 +1,28 @@
+import { AppBar, styled, Toolbar } from "@mui/material";
+import MenuOptions from "./MenuOptions";
+import Logo from "./Logo";
+
+const StyledAppBar = styled(AppBar)(({ theme }) => ({
+  backgroundColor: theme.palette.secondary.main,
+  color: theme.palette.text.primary,
+  boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.2)",
+  padding: "0 20px",
+}));
+
+const StyledToolbar = styled(Toolbar)(({ theme }) => ({
+  backgroundColor: theme.palette.secondary.main,
+  color: theme.palette.text.primary,
+}));
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <StyledAppBar position="static">
+      <StyledToolbar disableGutters>
+        <Logo />
+        <MenuOptions />
+      </StyledToolbar>
+    </StyledAppBar>
+  );
 };
 
 export default Navbar;
